75fe3a75baa51b3a3f8bc739a5705e15e8435b65
Michael Burton <micbur1@gmail.com>
2021-08-03 00:52:09 -0700

    add basic functionallity for stop watch along with buttons to start, stop, reset, and resume

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
 function App() {
+
+  const [time, setTime] = React.useState(0)
+  const [timerOn, setTimerOn] = React.useState(false)
+
+  React.useEffect(() => {
+    let interval = null;
+
+    if (timerOn) {
+      interval = setInterval(() => {
+        setTime(prevTime => prevTime + 10) //makes time equal to previouse time and increas the interval every 10 milliseconds (accounts for 100th of a sec). while timer is on
+      }, 10)
+    } else {
+      clearInterval(interval)// CLearInterval() = javascript method
+    }
+
+    // Clean-up function (not needed, but better code ) will stop the interval when the user leaves the page to "stop memory leaks"
+    return () => clearInterval(interval)
+
+  }, [timerOn])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>{time}</div>
+      <div>
+        <button onClick={() => setTimerOn(true)} >Start</button>
+        <button onClick={() => setTimerOn(false)}  >Stop</button>
+        <button onClick={() => setTimerOn(true)} >Resume</button>
+        <button onClick={() => setTime(0)} >Reset</button>
+      </div>
     </div>
   );
 }
